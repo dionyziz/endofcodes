6c98cf564a1c4f6d57293e00a14d6f4f53bf814b
Vitalis Salis <vitsalis@gmail.com>
2013-11-03 13:28:13 +0200

    Removed htmlspecialchars from "UseController"

--- controllers/user.php
+++ controllers/user.php
@@ -3,9 +3,9 @@
         public static function create() {
             include 'models/users.php';
             if ( !empty( $_POST[ 'username' ] ) && !empty( $_POST[ 'password' ] ) && !empty( $_POST[ 'email' ] ) ) {
-                $username = htmlspecialchars( $_POST[ 'username' ] );
-                $password = htmlspecialchars( $_POST[ 'password' ] );
-                $email = htmlspecialchars( $_POST[ 'email' ] );
+                $username = $_POST[ 'username' ];
+                $password = $_POST[ 'password' ];
+                $email = $_POST[ 'email' ];
                 $posat = strrpos( $email, "@" );
                 $posdot = strrpos( $email, "." );
                 if ( $posat < 1 || $posat === false || $posdot == strlen( $email ) || $posdot === false ) {
