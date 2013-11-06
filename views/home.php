973139a805edd001b9d3d06ab898081927b66382
Vitalis Salis <vitsalis@gmail.com>
2013-11-06 17:44:37 +0200

    Added hello $user

--- views/home.php
+++ views/home.php
@@ -1,6 +1,7 @@
 <?php
     if ( isset( $username ) ) {
-        ?><a href="index.php?resource=session&amp;method=delete">Logout</a><?php
+        ?><p>Hello, <?php echo $username; ?>.</p>
+        <p><a href="index.php?resource=session&amp;method=delete">Logout</a></p><?php
     }
     else {
         ?><a href="index.php?resource=session&amp;method=create">Login</a> or <a href="index.php?resource=user&amp;method=create">Register</a><?php
