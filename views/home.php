9638415732d34ba940a6f932e149e1ba6ffcdf60
Vitalis Salis <vitsalis@gmail.com>
2013-11-09 11:02:49 +0200

    Added logoutform.php for security purposes

--- views/home.php
+++ views/home.php
@@ -1,7 +1,10 @@
 <?php
     if ( isset( $username ) ) {
         ?><p>Hello, <?php echo $username; ?>.</p>
-        <p><a href="index.php?resource=session&amp;method=delete">Logout</a></p><?php
+        <?php
+            include 'logoutform.php';
+        ?>
+        <?php
     }
     else {
         ?><a href="index.php?resource=session&amp;method=create">Login</a> or <a href="index.php?resource=user&amp;method=create">Register</a><?php
---
+++ views/logoutform.php
@@ -0,0 +1,3 @@
+<form action="index.php?resource=session&amp;method=delete" method="POST">
+    <input type="submit" value="Logout" />
+</form>
