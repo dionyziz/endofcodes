fa74e1b76a0f589712a3a060a2c3d9454e537600
Vitalis Salis <vitsalis@gmail.com>
2013-11-14 16:53:13 +0200

    Added HTTPErrorException class

--- header.php
+++ header.php
@@ -10,4 +10,30 @@
             $this->url = $url;
         }
     }
+
+    class HTTPErrorException extends Exception {
+        public $header;
+
+        public function __construct( $error, $description= "" ) {
+            if ( !empty( $description ) ) {
+                parent::__construct( "HTTP/1.1 $error $description" );
+            }
+            else {
+                $this->header = "HTTP/1.1 $error"
+                parent::__construct( "HTTP/1.1 $error" );
+            }
+        }
+    }
+
+    class HTTPNotFoundException extends HTTPErrorException {
+        public function __construct() {
+            parent::__construct( '404', 'Not Found' );
+        }
+    }
+
+    class HTTPUnauthorizedException extends HTTPErrorException {
+        public function __construct() {
+            parent::__construct( '401' );
+        }
+    }
 ?>
