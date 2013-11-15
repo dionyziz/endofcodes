7d655c7c5addf4716f433ca9aaf3cb54887ef232
Vitalis Salis <vitsalis@gmail.com>
2013-11-15 13:20:52 +0200

    Removed function Mail::exists() because I do not use it

--- models/mail.php
+++ models/mail.php
@@ -1,22 +1,5 @@
 <?php
     class Mail {
-        public function mailExists( $mail ) {
-            $res = db(
-                'SELECT
-                    username
-                FROM
-                    users
-                WHERE
-                    email = :mail
-                LIMIT 1;', 
-                compact( "mail" ) 
-            );
-            if ( mysql_num_rows( $res ) == 1 ) {
-                return true;
-            }
-            return false;
-        }
-
         public function validMail( $mail ) {
             $posat = strrpos( $mail, "@" );
             $posdot = strrpos( $mail, "." );
