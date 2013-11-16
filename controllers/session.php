3e42239bdd04cc8a9388ef32f9603e242401fae9
Vitalis Salis <vitsalis@gmail.com>
2013-11-16 06:36:45 +0200

    Reformat session.php

--- controllers/session.php
+++ controllers/session.php
@@ -2,20 +2,21 @@
     class SessionController {
         public static function create( $username = '', $password = '' ) {
             include 'models/users.php';
-            if ( !empty( $username ) && !empty( $password ) ) {
-                if ( $id = User::authenticateUser( $username, $password ) ) {
-                    $_SESSION[ 'user' ] = array(
-                        'userid' => $id,
-                        'username' => $username
-                    );
-                    throw new RedirectException( 'index.php?resource=dashboard&method=view' );
-                }
-                throw new RedirectException( 'index.php?resource=session&method=create&error=yes' );
-            }
             if ( empty( $username ) ) {
                 throw new RedirectException( 'index.php?empty_user=yes&resource=session&method=create' );
             }
-            throw new RedirectException( 'index.php?empty_pass=yes&resource=session&method=create' );
+            if ( empty( $password ) ) {
+                throw new RedirectException( 'index.php?empty_pass=yes&resource=session&method=create' );
+            }
+            $id = User::authenticateUser( $username, $password );
+            if ( $id == false ) {
+                throw new RedirectException( 'index.php?resource=session&method=create&error=yes' );
+            }
+            $_SESSION[ 'user' ] = array(
+                'userid' => $id,
+                'username' => $username
+            );
+            throw new RedirectException( 'index.php?resource=dashboard&method=view' );
         }
 
         public static function delete() {
