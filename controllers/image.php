2318905b52ce2b449e0c81e1881141378db5669c
Vitalis Salis <vitsalis@gmail.com>
2013-11-16 06:29:16 +0200

    Remove unnecessary else

--- controllers/image.php
+++ controllers/image.php
@@ -7,12 +7,10 @@
             $avatarname = basename( $image[ 'name' ] );
             $tmp_name = $image[ 'tmp_name' ];
             $id = $_SESSION[ 'user' ][ 'userid' ];
-            if ( isset( $_SESSION[ 'user' ][ 'username' ] ) ) {
-                $username = $_SESSION[ 'user' ][ 'username' ];
-            }
-            else {
+            if ( !isset( $_SESSION[ 'user' ][ 'username' ] ) ) {
                 throw new HTTPUnauthorizedException();
             }
+            $username = $_SESSION[ 'user' ][ 'username' ];
             try {
                 Image::create( $username, $tmp_name, $avatarname, $id );
             }
