854450d05bd401c32a8af697c3f65c556597318f
Vitalis Salis <vitsalis@gmail.com>
2013-11-29 17:57:10 +0200

    Adds the header of the page

--- views/header.php
+++ views/header.php
@@ -5,3 +5,15 @@
         <title>EndofCodes Demo</title>
     </head>
     <body>
+        <ul>
+            <li><a href="index.php">Endofcodes</a></li>
+            <li><a href="">Rules</a></li>
+            <?php
+                if ( isset( $_SESSION[ 'user' ]['username' ] ) ) {
+                    ?><li><a href="index.php?resource=user&amp;method=view&amp;username=<?php
+                        echo htmlspecialchars( $_SESSION[ 'user' ][ 'username' ] );
+                    ?>">Profile</a></li><?php
+                }
+            ?>
+            <li><a href="http://blog.endofcodes.com">Blog</a></li>
+        </ul>
