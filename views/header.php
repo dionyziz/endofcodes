e6d71b0eca236891ecf12505a0ad7b6370db0a2b
Vitalis Salis <vitsalis@gmail.com>
2013-11-30 11:25:38 +0200

    Adds some CSS and HTML to the header

--- views/header.php
+++ views/header.php
@@ -3,31 +3,39 @@
 <html>
     <head>
         <title>EndofCodes Demo</title>
+        <link 
+            rel="stylesheet" 
+            type="text/css" 
+            href="static/style/header.css" />
+        <meta 
+            http-equiv="Content-type" 
+            content="text/html; charset=utf-8" />
     </head>
     <body>
-        <ul>
-            <li><a href="index.php">Endofcodes</a></li>
-            <li><a href="">Rules</a></li>
-            <?php
-                if ( isset( $_SESSION[ 'user' ]['username' ] ) ) {
-                    ?><li><a href="index.php?resource=user&amp;method=view&amp;username=<?php
-                        echo htmlspecialchars( $_SESSION[ 'user' ][ 'username' ] );
-                    ?>">Profile</a></li><?php
-                }
-            ?>
-            <li><a href="http://blog.endofcodes.com">Blog</a></li>
-        </ul>
-        <?php
-            if ( isset( $_SESSION[ 'user' ][ 'username' ] ) ) {
-                ?><p><?php
-                    echo htmlspecialchars( $_SESSION[ 'user' ][ 'username' ] );
-                ?></p>
-                <select>
-                    <option></option>
-                    <option value="logout">Logout</option>
-                </select><?php
-            }
-            else {
-                ?><p><a href="index.php?resource=session&amp;method=create">Login</a></p><?php
-            }
-        ?>
+        <div id="header">
+            <ul>
+                <li><a href="index.php">Endofcodes</a></li>
+                <li><a href="">Rules</a></li>
+                <?php
+                    if ( isset( $_SESSION[ 'user' ]['username' ] ) ) {
+                        ?><li><a href="index.php?resource=user&amp;method=view&amp;username=<?php
+                            echo htmlspecialchars( $_SESSION[ 'user' ][ 'username' ] );
+                        ?>">Profile</a></li><?php
+                    }
+                ?>
+                <li><a href="http://blog.endofcodes.com">Blog</a></li>
+                <?php
+                    if ( isset( $_SESSION[ 'user' ][ 'username' ] ) ) {
+                        ?><li id="login"><?php
+                            echo htmlspecialchars( $_SESSION[ 'user' ][ 'username' ] );
+                        ?><select>
+                            <option> </option>
+                            <option value="logout">Logout</option>
+                        </select></li><?php
+                    }
+                    else {
+                        ?><li id="login"><a href="index.php?resource=session&amp;method=create">Login</a></li><?php
+                    }
+                ?>
+            </ul>
+        </div>
