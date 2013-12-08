91d3b4cb66aaca0f721cc5e96e97ab812de1264b
Vitalis Salis <vitsalis@gmail.com>
2013-12-08 19:26:44 +0200

    Make config a global variable

--- controllers/image.php
+++ controllers/image.php
@@ -7,6 +7,7 @@
                 throw new HTTPUnauthorizedException();
             }
             $config = getConfig();
+            global $config;
             $name = $image[ 'name' ];
             $tmp_name = $image[ 'tmp_name' ];
             $user = User::find_by_username( $_SESSION[ 'user' ][ 'username' ] );
