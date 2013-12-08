d7ed1c48f533eccdd7ad53408c20de3dca6c6eea
Vitalis Salis <vitsalis@gmail.com>
2013-12-08 17:26:36 +0200

    Use fixed queries for population script

--- database/population/country.php
+++ database/population/country.php
@@ -1,26 +1,21 @@
 <?php
     include_once '../../config/config-local.php';
     include_once '../../models/database.php';
+    include_once '../../models/db.php';
     include_once 'countries_array.php';
     
+    $countries = getCountries();
     $array = array();
     $count = 0;
     $keys = array_keys( $countries );
   
     foreach ( $keys as $key ) {
         $value = $countries[ $key ];
-        $array[] = "INSERT INTO 
-                        `countries` (`id`, `country`, `shortname`) 
-                    VALUES 
-                        (NULL, '$value', '$key');";
-    }
-    
-    foreach( $array as $sql ) {
-        $res = mysql_query( $sql );
-        if ( $res === false ) {
+        $res = db_insert( 'countries', array( 'id' => NULL, 'country' => $value, 'shortname' => $key ) );
+        if ( $res === false ) { 
             die( "sql query died with the following error\n\"" . mysql_error() );
         }
-        ++$count; 
+        ++$count;
     }
     
     echo "You imported $count raws in the table 'countries'.";
