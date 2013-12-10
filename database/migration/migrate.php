b02aed4339f57281188e31fcd2f69e2d3ab087ac
Vitalis Salis <vitsalis@gmail.com>
2013-12-10 13:14:58 +0200

    I use db() instead of mysql_array

--- database/migration/migrate.php
+++ database/migration/migrate.php
@@ -2,9 +2,10 @@
     function migrate( $sql_array = array() ) {
         include_once '../../config/config-local.php'; 
         include_once '../../models/database.php';
+        include_once '../../models/db.php';
 
         foreach ( $sql_array as $sql ) {
-            $res = mysql_query( $sql );
+            $res = db( $sql );
             if ( $res === false ) {
                 die( 'Migration failed. SQL query died with the following error: ' . mysql_error() );
             }
