0401cfaf9b0ee039caba3ea09757b9d56394822a
Vitalis Salis <vitsalis@gmail.com>
2014-01-15 21:30:05 +0200

    Add ACTION_NONE to the definitions

--- models/intent.php
+++ models/intent.php
@@ -4,8 +4,9 @@
     define( 'DIRECTION_EAST', 2 );
     define( 'DIRECTION_SOUTH', 3 );
     define( 'DIRECTION_WEST', 4 );
-    define( 'ACTION_ATACK', 0 );
+    define( 'ACTION_NONE', 0 );
     define( 'ACTION_MOVE', 1 );
+    define( 'ACTION_ATACK', 2 );
     class Intent {
         public $action;
         public $direction;
@@ -19,8 +20,9 @@
                 'DIRECTION_WEST' => DIRECTION_WEST
             )[ $direction ];
             $this->action = array(
-                'ACTION_ATACK' => ACTION_ATACK,
-                'ACTION_MOVE' => ACTION_MOVE
+                'ACTION_NONE' => ACTION_NONE,
+                'ACTION_MOVE' => ACTION_ATACK,
+                'ACTION_ATACK' => ACTION_MOVE
             )[ $action ];
         }
     }
