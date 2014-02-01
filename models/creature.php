204ba777b340a6faa55fe7db085f370370fa67b4
Vitalis Salis <vitsalis@gmail.com>
2014-02-01 14:51:07 +0200

    Make finding alive more stylish :p

--- models/creature.php
+++ models/creature.php
@@ -20,9 +20,7 @@
                 $this->locationx = $creature_info[ 'locationx' ];
                 $this->locationy = $creature_info[ 'locationy' ];
                 $this->hp = $creature_info[ 'hp' ];
-                if ( $this->hp > 0 ) {
-                    $this->alive = true;
-                }
+                $this->alive = $this->hp > 0;
                 $action = actionStringToConst( $creature_info[ 'action' ] );
                 $direction = directionStringToConst( $creature_info[ 'direction' ] );
                 $this->intent = new Intent( $action, $direction );
