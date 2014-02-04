65f3ea4fc7a64e4ec5a87cef91848e777ad27332
pkakelas <pkakelas@gmail.com>
2014-02-04 20:09:13 +0200

    Add testInsertCountries()

--- tests/models/country.php
+++ tests/models/country.php
@@ -18,11 +18,16 @@
                 $country->save();
             }
         }
+        public function testInsertCountries() {
+            $this->insertCountries();
+            $countries = Country::findAll();
+            $this->assertTrue( !empty( $countries ), "'insertCountries()' did not insert countries in the db" );
+        }
         public function testFindAll() {
             $this->insertCountries();
             $countriesArray = Country::findAll(); 
             $this->assertTrue( is_array( $countriesArray ), 'Country::findAll() did not return an array of countries' );
-            $this->assertTrue( count( $countriesArray ) > 3, 'Country::findAll() did not return as many countries as it should' );
+            $this->assertTrue( count( $countriesArray ) >= 3, 'Country::findAll() did not return as many countries as it should' );
         } 
         public function testCountryConstruct() {
             $countries = $this->countries;
@@ -31,6 +36,7 @@
             $shortname = $shortnames[ 0 ];
             $this->insertCountries();
             $country = new Country( $testId );
+            $this->assertTrue( is_object( $country ), "'new Country()' did not return an object" );
             $this->assertEquals( $testId, $country->id, "'new Country()' did not return the appropriate country" );
             $this->assertEquals( $country->shortname, $shortname, "'new Country()' did not return the appropriate shortname" );
             $this->assertEquals( $country->name, $countries[ $shortname ], "'new Country()' did not return the appropriate name" );
