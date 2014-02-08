f4f2256591f810cce372d6e3a253ad06edd342bf
Dionysis Zindros <dionyziz@gmail.com>
2014-02-08 16:08:43 +0200

    Add some better errors for database problems

--- models/database.php
+++ models/database.php
@@ -2,8 +2,8 @@
     function dbInit() {
         global $config;
 
-        mysql_connect( $config[ 'db' ][ 'host' ], $config[ 'db' ][ 'user' ], $config[ 'db' ][ 'pass' ] );
+        mysql_connect( $config[ 'db' ][ 'host' ], $config[ 'db' ][ 'user' ], $config[ 'db' ][ 'pass' ] ) or die( 'Failed to connect to MySQL: ' . mysql_error() );
 
-        mysql_select_db( $config[ 'db' ][ 'dbname' ] );
+        mysql_select_db( $config[ 'db' ][ 'dbname' ] ) or die( 'Failed to select MySQL database: ' . mysql_error() );
     }
 ?>
