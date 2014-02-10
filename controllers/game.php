aba1574edcf37a1bfce2916e4fcfce93f7fcc3cc
Vitalis Salis <vitsalis@gmail.com>
2014-02-10 16:19:54 +0200

    Add controller for game

---
+++ controllers/game.php
@@ -0,0 +1,21 @@
+<?php
+    include_once 'models/grader/grader.php';
+
+    class GameController extends ControllerBase {
+        public function create() {
+            $game = new Game();
+            $users = User::findAll();
+            $grader = new Grader( $users, $game );
+            $grader->initiate();
+            $grader->createGame();
+            $grader->game->genesis();
+        }
+        public function createView() {
+            include 'views/game/create.php';
+        }
+        public function update( $grader ) {
+            $grader->nextRound();
+            $grader->game->nextRound();
+        }
+    }
+?>
