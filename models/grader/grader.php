211289c4654459ec0ff634c9d28ecc83f220851b
Vitalis Salis <vitsalis@gmail.com>
2014-02-10 16:18:58 +0200

    Add construct and make bots attribute protected

--- models/grader/grader.php
+++ models/grader/grader.php
@@ -4,10 +4,18 @@
     include_once 'models/grader/bot.php';
 
     class Grader {
-        public $bots;
         public $registeredBots;
         protected $game;
+        protected $users;
+        protected $bots = [];
 
+        public function __construct( $users, $game ) {
+            $this->users = $users;
+            foreach ( $users as $user ) {
+                $this->bots[] = new GraderBot( $user );
+            }
+            $this->game = $game;
+        }
         public function initiate() {
             $this->registeredBots = [];
             foreach ( $this->bots as $bot ) {
@@ -20,13 +28,17 @@
             }
         }
         public function createGame() {
-            foreach ( $this->bots as $bot ) {
+            $this->game->users = $this->users;
+            $this->game->save();
+
+            foreach ( $this->registeredBots as $bot ) {
                 $bot->sendGameRequest( $this->game );
             }
         }
         public function nextRound() {
             $round = $this->game->getCurrentRound();
-            foreach ( $this->bots as $bot ) {
+
+            foreach ( $this->registeredBots as $bot ) {
                 $bot->sendRoundRequest( $round );
             }
         }
