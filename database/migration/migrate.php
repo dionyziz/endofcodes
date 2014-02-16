91799063d84616fa6527be73a0cb5babc9624ece
pkakelas <pkakelas@gmail.com>
2014-02-16 15:58:03 +0200

    Add unique and primary keys in createTable arguments

--- database/migration/migrate.php
+++ database/migration/migrate.php
@@ -43,17 +43,31 @@
             self::migrate( $sql );
         }
 	
-	    public static function createTable( $tableName, $fields = [] ) {
-		    $attributes = [];
+	    public static function createTable( $tableName, $fields = [], $keys = [] ) {
 		    foreach ( $fields as $field => $description ) {
 			    $attributes[] = "$field $description";
 		    }
-		    $attributes = implode( ',', $attributes );
-		    $sql = "CREATE TABLE IF NOT EXISTS
-			        $tableName (
-				        $attributes
-			        )
-			        ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;";
+            if ( !empty( $keys ) ) {
+                foreach ( $keys as $key => $value ) {
+                    if ( $key == 'unique' ) {
+                        foreach ( $value as $string ) {
+                            $args[] = "UNIQUE KEY $string";
+                        }
+                    } 
+                    else {
+                        foreach ( $value as $string ) {
+                            $args[] = "PRIMARY KEY $string";
+                        }
+                    }
+                }
+                $attributes = array_merge( $attributes, $args );
+            } 
+            $attributes = implode( ',', $attributes );
+            $sql = "CREATE TABLE IF NOT EXISTS
+                $tableName (
+                    $attributes
+                )
+                ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;";
 		    self::migrate( $sql );
 	    }
     }
