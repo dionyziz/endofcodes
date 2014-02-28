7d61008cb5f45e75b648d1ecea7ed8e84da53932
pkakelas <pkakelas@gmail.com>
2014-02-28 19:22:26 +0200

    Change last migration to new mig format

--- database/migration/20140228155432_change_enum_of_action.php
+++ database/migration/20140228155432_change_enum_of_action.php
@@ -1,12 +1,5 @@
 <?php
     require_once 'migrate.php';
 
-    migrate(
-        [
-            'ALTER TABLE
-                roundcreatures
-            CHANGE
-                `action` `action` ENUM("NONE","MOVE","ATTACK") COLLATE utf8_unicode_ci NOT NULL'
-        ]
-    );
+    Migration::alterField( 'roundcreatures', 'action', 'action', 'ENUM("NONE","MOVE","ATTACK") COLLATE utf8_unicode_ci NOT NULL' );
 ?>
