657f18f7dbceb589ca390eab347edd844e7c42b6
pkakelas <pkakelas@gmail.com>
2014-02-28 15:10:16 +0200

    Corrections

--- database/migration/20131224000228_change_gameusers_pk.php
+++ database/migration/20131224000228_change_gameusers_pk.php
@@ -1,5 +1,5 @@
 <?php
-    require 'migrate.php';
+    require_once 'migrate.php';
 
     Migration::dropIndex( 'gameusers', 'uc_gameusers' );
     Migration::addPrimaryKey( 'gameusers', 'pk_gameusers', [ 'gameid', 'userid' ] );
