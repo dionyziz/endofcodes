79f91cc6f14bbe9d156b04fe4ec7aa5ffbec6f0d
pkakelas <pkakelas@gmail.com>
2014-02-28 15:49:38 +0200

    Coding style

--- database/migration/00000000000000_init_db.php
+++ database/migration/00000000000000_init_db.php
@@ -2,14 +2,14 @@
     require_once 'migrate.php';
 
     Migration::createTable( 
-		'users', 
+        'users', 
         [
             'id' => 'int(11) NOT NULL AUTO_INCREMENT',
-		    'username' => 'varchar(50) COLLATE utf8_unicode_ci NOT NULL',
-		    'password' => 'varchar(200) COLLATE utf8_unicode_ci NOT NULL',
-		    'email' => 'varchar(200) COLLATE utf8_unicode_ci NOT NULL',
-		    'salt' => 'varchar(200) COLLATE utf8_unicode_ci NOT NULL',
-		    'avatarid' => 'int(11) NOT NULL'
+            'username' => 'varchar(50) COLLATE utf8_unicode_ci NOT NULL',
+            'password' => 'varchar(200) COLLATE utf8_unicode_ci NOT NULL',
+            'email' => 'varchar(200) COLLATE utf8_unicode_ci NOT NULL',
+            'salt' => 'varchar(200) COLLATE utf8_unicode_ci NOT NULL',
+            'avatarid' => 'int(11) NOT NULL'
         ],
         [ 
             [ 'type' => 'unique', 'field' => [ 'username', 'email' ] ],
@@ -18,11 +18,11 @@
     );
     
     Migration::createTable( 
-		'images', 
+        'images', 
         [
             'imageid' => 'int(11) NOT NULL AUTO_INCREMENT',
-		    'userid' => 'int(11) NOT NULL',
-		    'imagename' => 'varchar(200) COLLATE utf8_unicode_ci NOT NULL',
+            'userid' => 'int(11) NOT NULL',
+            'imagename' => 'varchar(200) COLLATE utf8_unicode_ci NOT NULL',
         ],
         [ 
             [ 'type' => 'primary', 'field' => ['imageid'] ]
