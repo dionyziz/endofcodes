46a0a89cc8953452e52649a5ec05681bb63416ce
pkakelas <pkakelas@gmail.com>
2014-03-01 17:51:15 +0200

    Create bot update controller to save boturl

--- controllers/bot.php
+++ controllers/bot.php
@@ -1,9 +1,17 @@
 <?php
     class BotController extends ControllerBase {
-        public function update( $boturl ) {
-            echo 'AWESOME BITCH. IT IS WORKING!!';  
+        public function update( $boturl = '' ) {
+            if ( empty( $boturl ) ) {
+                go( 'bot', 'update', [ 'boturl_empty' => true ] );
+            }
+            if ( isset( $_SESSION[ 'user' ] ) ) {
+                $user = $_SESSION[ 'user' ];
+            }
+            $user->boturl = $boturl; 
+            $user->save();
         }
-        public function updateView( $boturl_empty, $boturl_invalid ) {
+        public function updateView( $boturl_empty, $boturl_invalid, $bot_success, $bot_not_success, $invalid_hostname, $invalid_ip, 
+                $port_forward_incorrect, $initiation_incorrect, $json_incorrect, $invalid_json_dictionary, $username_incorrect     ) {
             require_once 'views/bots/update.php'; 
         } 
     }
--- views/bots/update.php
+++ views/bots/update.php
@@ -2,30 +2,30 @@
     require 'views/header.php';?>
     
     <p>To begin playing, you must set up your bot.<a href=''> Start by reading the tutorial.</a></p><?php
-    if ( isset($bot_success ) ) {
+    if ( $bot_success ) {
         ?><p>Your bot is correctly configured <img src='static/images/check.png' /></p><?php
     }
-    else if ( isset( $bot_not_success ) ) {
-        ?><p>Your bot is incorrectly configured <img src='static/images/cancel.png' /></p><?php
-        if ( isset( $invalid_hostname ) ) {
+    else if ( $bot_not_success ) {
+        ?><p>Yur bot is incorrectly configured <img src='static/images/cancel.png' /></p><?php
+        if ( $invalid_hostname ) {
             ?><p>Your bot hostname is invalid. Did you enter a valid hostname?</p><?php
         }
-        if ( isset( $invalid_ip ) ) {
+        if ( $invalid_ip ) {
             ?><p>Your bot is unreachable on the network. Did you enter your public IP address?</p><?php
         }
-        if ( isset( $port_forward_incorrect ) ) {
+        if ( $port_forward_incorrect ) {
             ?><p>Your bot is refusing connections. Did you port forward correctly?</p><?php
         }
-        if ( isset( $initiation_incorrect ) ) {
+        if ( $initiation_incorrect ) {
             ?><p>Your bot is running, but not responding to initiation. Did you write code to handle initiation?</p><?php
         }
-        if ( isset( $json_incorrect ) ) {
+        if ( $json_incorrect ) {
             ?><p>Your bot is not sending valid JSON. Did you write code to generate JSON correctly?</p><?php
         }
-        if ( isset( $invalid_json_dictionary ) ) {
+        if ( $invalid_json_dictionary ) {
             ?><p>You must set the bot name, version, and your username. Did you build the correct JSON dictionary?</p><?php
         }
-        if ( isset( $username_incorrect ) ) {
+        if ( $username_incorrect ) {
             ?><p>Your bot is not using your username. Did you set your username correctly?</p><?php
         }
     }
