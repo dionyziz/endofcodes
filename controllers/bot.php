91899fb03842d0d8dd1fe2f21f487556a00a19d0
pkakelas <pkakelas@gmail.com>
2014-03-01 19:21:59 +0200

    Create part of boturl validation in controller

--- controllers/bot.php
+++ controllers/bot.php
@@ -1,6 +1,7 @@
 <?php
     class BotController extends ControllerBase {
         public function update( $boturl = '' ) {
+            require_once 'models/grader/bot.php';
             if ( empty( $boturl ) ) {
                 go( 'bot', 'update', [ 'boturl_empty' => true ] );
             }
@@ -8,10 +9,34 @@
                 $user = $_SESSION[ 'user' ];
             }
             $user->boturl = $boturl; 
+            $bot = new GraderBot( $user );
+            try {
+                $bot->sendInitiateRequest(); 
+            }
+            catch ( GraderBotException $e ) {
+                //switch ( $e ) {
+                //    case 'initiate_could_not_resolve': 
+                //        $error = $invalid_hostname;
+                //        break; 
+                //    case 'initiate_could_not_connect':
+                //        $error = $bot_unreachable;
+                //       break;
+                //    case 'initiate_invalid_json': 
+                //        $error = $invalid_json;
+                //        break; 
+                //    case 'initiate_username_mismatch':
+                //       $error = $username_mismatch;
+                //        break;
+                //} 
+                $error = str_replace( "initiate_","",$url );
+                go( 'bot', 'update', [ $error => true ] );
+            }
             $user->save();
+            go( 'bot', 'update', [ 'bot_success' => true ] );
         }
-        public function updateView( $boturl_empty, $boturl_invalid, $bot_success, $bot_not_success, $invalid_hostname, $invalid_ip, 
-                $port_forward_incorrect, $initiation_incorrect, $json_incorrect, $invalid_json_dictionary, $username_incorrect     ) {
+        public function updateView( $boturl_empty, $boturl_invalid, $bot_success, $bot_not_success, $could_not_resolve, 
+                $could_not_connect, $username_mismatch, $port_forward_incorrect, $initiation_incorrect, $invalid_json, 
+                $invalid_json_dictionary ) {
             require_once 'views/bots/update.php'; 
         } 
     }
--- views/bots/update.php
+++ views/bots/update.php
@@ -3,30 +3,30 @@
     
     <p>To begin playing, you must set up your bot.<a href=''> Start by reading the tutorial.</a></p><?php
     if ( $bot_success ) {
-        ?><p>Your bot is correctly configured <img src='static/images/check.png' /></p><?php
+        ?><p class='error'>Your bot is correctly configured <img src='static/images/check.png' /></p><?php
     }
     else if ( $bot_not_success ) {
-        ?><p>Yur bot is incorrectly configured <img src='static/images/cancel.png' /></p><?php
-        if ( $invalid_hostname ) {
-            ?><p>Your bot hostname is invalid. Did you enter a valid hostname?</p><?php
+        ?><p class='error'>Your bot is incorrectly configured <img src='static/images/cancel.png' /></p><?php
+        if ( $could_not_resolve ) {
+            ?><p class='error'>Your bot hostname is invalid. Did you enter a valid hostname?</p><?php
         }
-        if ( $invalid_ip ) {
-            ?><p>Your bot is unreachable on the network. Did you enter your public IP address?</p><?php
+        if ( $could_not_connect ) {
+            ?><p class='error'>Your bot is unreachable on the network. Did you enter your public IP address?</p><?php
         }
         if ( $port_forward_incorrect ) {
-            ?><p>Your bot is refusing connections. Did you port forward correctly?</p><?php
+            ?><p class='error'>Your bot is refusing connections. Did you port forward correctly?</p><?php
         }
         if ( $initiation_incorrect ) {
-            ?><p>Your bot is running, but not responding to initiation. Did you write code to handle initiation?</p><?php
+            ?><p class='error'>Your bot is running, but not responding to initiation. Did you write code to handle initiation?</p><?php
         }
-        if ( $json_incorrect ) {
-            ?><p>Your bot is not sending valid JSON. Did you write code to generate JSON correctly?</p><?php
+        if ( $invalid_json ) {
+            ?><p class='error'>Your bot is not sending valid JSON. Did you write code to generate JSON correctly?</p><?php
         }
         if ( $invalid_json_dictionary ) {
-            ?><p>You must set the bot name, version, and your username. Did you build the correct JSON dictionary?</p><?php
+            ?><p class='error'>You must set the bot name, version, and your username. Did you build the correct JSON dictionary?</p><?php
         }
-        if ( $username_incorrect ) {
-            ?><p>Your bot is not using your username. Did you set your username correctly?</p><?php
+        if ( $username_mismatch ) {
+            ?><p class='error'>Your bot is not using your username. Did you set your username correctly?</p><?php
         }
     }
     ?><p>API key: <?php echo $apikey ?></p><?php
