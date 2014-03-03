d35badbca55175e10ea388e2d65b2a8c488673f6
Vitalis Salis <vitsalis@gmail.com>
2014-03-03 18:44:19 +0200

    Change botname

--- bots/php/bot.php
+++ bots/php/bot.php
@@ -1,7 +1,7 @@
 <?php
     echo json_encode( [
-        'botname' => 'suprabot',
+        'botname' => 'botname',
         'version' => '0.1.0',
-        'username' => 'vitsalis'
+        'username' => 'user'
     ] );
 ?>
