686b8cf4e0576233067fcadb2d564b57db4f04e0
pkakelas <pkakelas@gmail.com>
2014-03-06 22:46:18 +0200

    Made custom ol from php

--- views/home.php
+++ views/home.php
@@ -3,24 +3,26 @@
 ?>
 <div id="home">
     <p class='center'>Last match ratings(global)</p>
-    <ol class='center' id="toplist">
+    <ul class='center' id="toplist">
         <?php
+            $i = 1;
             foreach( $ratings as $rating ) {
                 foreach ( $rating as $user ) {
-                    ?><li><a href=""><?php echo $user->username; ?></a></li><?php
+                    ?><li><a href=""><?php echo "$i.$user->username"; ?></a></li><?php
                 }
+                $i++;
             } 
         ?>
-    </ol>
+    </ul>
     <?php
-//        $filter = [ 
-//            [ 'value' => 'default', 'content' => 'select Ratings' ], 
-//            [ 'value' => 'global', 'content' => 'Global' ], 
-//            [ 'value' => 'country', 'content' => 'By country' ]
-//        ];
-//        $form = new Form( 'dashboard', 'view' );
-//        $form->id = 'selectbox';
-//        $form->createSelect( 'filter', '', $filter );
+//      $filter = [ 
+//          [ 'value' => 'default', 'content' => 'select Ratings' ], 
+//          [ 'value' => 'global', 'content' => 'Global' ], 
+//          [ 'value' => 'country', 'content' => 'By country' ]
+//      ];
+//      $form = new Form( 'dashboard', 'view' );
+//      $form->id = 'selectbox';
+//      $form->createSelect( 'filter', '', $filter );
         if ( isset( $user ) ) {
             require_once 'views/session/logoutform.php';
         }
