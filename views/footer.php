1db5073fb39ba1a7d7f1547feffb0bf70e08e55d
pkakelas <pkakelas@gmail.com>
2014-03-07 21:11:53 +0200

    Fixed footer

--- views/footer.php
+++ views/footer.php
@@ -1,6 +1,5 @@
-        </div>
-        <footer class="footer" class="panel-footer"> 
-        <ul>
+        <footer id="footer" class="panel-footer"> 
+        <ul class="list-inline">
             <li><a href="">Rules</a></li>
             <li><a href="http://blog.endofcodes.com">Blog</a></li>
             <li><a href="https://github.com/dionyziz/endofcodes">Contribute</a></li>
