b99f9549bc083919c7bf6146c057e1e7796458f3
Vitalis Salis <vitsalis@gmail.com>
2014-03-07 16:42:02 +0200

    Add beforeNextRound function

--- controllers/game.php
+++ controllers/game.php
@@ -18,6 +18,7 @@
         }
         public function update( $gameid ) {
             $game = new Game( $gameid );
+            $game->beforeNextRound();
 
             $grader = new Grader( $game );
             try {
