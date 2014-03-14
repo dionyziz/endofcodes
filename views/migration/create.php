3accbff6d8a375bf2f1b7ffdcb05a39b780654b4
pkakelas <pkakelas@gmail.com>
2014-03-14 19:40:34 +0200

    Corrections

--- views/migration/create.php
+++ views/migration/create.php
@@ -1,33 +1,32 @@
 <?php
     require 'views/header.php';
-?>
-<?php
-        foreach ( $migrations as $migration ) {
-            $list[] = [
-                'content' => $migration,
-                'value' => $migration
-            ];
-        }
-        $form = new Form( 'migration', 'create' );
-        $form->output( function( $self ) use( $list ) {
 
-            $self->createSelect( 'name', '', $list );
-            $self->createSelect( 'env', 'env', [
-                [
-                    'content' => 'development',
-                    'value' => 'development'
-                ],
-                [
-                    'content' => 'test',
-                    'value' => 'test'
-                ]
-            ] );
-            $self->createSubmit( 'Run migration' );
-        } );
-        ?><p> Last Migration: <?php echo $last; ?></p><?php
+    foreach ( $migrations as $migration ) {
+        $list[] = [
+            'content' => $migration,
+            'value' => $migration
+        ];
+    }
+    $form = new Form( 'migration', 'create' );
+    $form->output( function( $self ) use( $list ) {
 
-        $form = new Form( 'migration', 'create' );
-        $form->output( function( $self ) use( $list ) {
+        $self->createSelect( 'name', '', $list );
+        $self->createSelect( 'env', 'env', [
+            [
+                'content' => 'development',
+                'value' => 'development'
+            ],
+            [
+                'content' => 'test',
+                'value' => 'test'
+            ]
+        ] );
+        $self->createSubmit( 'Run migration' );
+    } );
+    ?><p> Last Migration: <?php echo $last; ?></p><?php
+
+    $form = new Form( 'migration', 'create' );
+    $form->output( function( $self ) use( $list ) {
         $self->createSelect( 'env', 'env', [
             [
                 'content' => 'development',
@@ -39,9 +38,7 @@
             ]
         ] );
         $self->createSubmit( "Run Migrations that haven't been executed" );
-        } );
+    } );
 
-?>
-<?php
     require 'views/footer.php';
 ?>
