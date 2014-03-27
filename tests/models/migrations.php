f3a224e320b8cb1fedc35e2166990e0becab3bd8
pkakelas <pkakelas@gmail.com>
2014-03-27 22:22:05 +0200

    remove unescessary asserts and tests from migrations test

--- tests/models/migrations.php
+++ tests/models/migrations.php
@@ -118,9 +118,6 @@
             }, 'MigrationException', 'alterField must alter a field when called' );
             $fields = dbListFields( 'testTable' );
             $this->assertThrows( function () { 
-                Migration::alterField( 'testTable', 'test', 'int(11) NOT NULL AUTO_INCREMENT' );
-            }, 'MigrationException', 'alterField must not create a field when an attribute is missing' );
-            $this->assertThrows( function () { 
                 Migration::alterField( 'test', 'testold', 'testnew', 'int(11) NOT NULL AUTO_INCREMENT' );
             }, 'MigrationException', 'alterField must return an error when the table not exists' );  
             $this->assertFalse( in_array( 'testold', $fields ), 'the old field should not exist in the list of the fields' );
@@ -137,26 +134,9 @@
                 Migration::dropField( 'testTable', 'testfield' );
             }, 'MigrationException', 'dropField must drop a field when called' );
             $this->assertThrows( function() {
-                Migration::dropField( 'testTable' );
-            }, 'MigrationException', 'dropField must return an error when an attribute is missing' );
-            $this->assertThrows( function() {
                 Migration::dropField( 'test', 'test' );
             }, 'MigrationException', 'dropField must return an error when table not exists' );
             $this->assertFalse( in_array( 'test', $fields ), 'the thrown field should not exist in the list of the fields' );
-        }
-        public function testAddFieldNoFieldName()  {
-            $this->assertThrows( function() {
-                Migration::addField( 'testTable' ); 
-            }, 'MigrationException', 'addField must not create a field when fieldname is empty' );
-        }
-        public function testCreateTableNoFields() {
-            $this->assertThrows( function() {
-                Migration::createTable( 
-                    'test', 
-                    [],
-                    []
-                );
-            }, 'MigrationException', 'createTable must not create a table when field are empty' );
         }
     }
     return new MigrationsTest();
