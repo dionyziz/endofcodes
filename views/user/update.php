696bc7cd47845755cce7060b34c23fb90b551789
pkakelas <pkakelas@gmail.com>
2014-03-29 14:56:54 +0200

    refactor in user/update

--- views/user/update.php
+++ views/user/update.php
@@ -3,51 +3,51 @@
 ?>
 
 <div class="text-center"><?php
-    if( isset( $valid_bot ) ) {
+    if( isset( $validBot ) ) {
         ?><p>Your bot is set up to play the next game</p><?php
     }
     ?><p><a href='bot/update'>Configure Bot.</a></p><?php
     $form = new Form( 'user', 'update' );
-    $form->output( function( $self ) use( $email_invalid, $email_used, $password_wrong,
-                                          $password_new_not_matched, $password_new_small, $countries, $user ) {
+    $form->output( function( $self ) use( $emailInvalid, $emailUsed, $passwordWrong,
+                                          $passwordNewNotMatched, $passwordNewSmall, $countries, $user ) {
         ?><p>Change email</p><?php
         $self->createLabel( 'email', 'Email' );
-        if ( isset( $email_invalid ) ) {
+        if ( isset( $emailInvalid ) ) {
             $self->createError( 'Email is not valid' );
         }
-        if ( isset( $email_used ) ) {
+        if ( isset( $emailUsed ) ) {
             $self->createError( 'Email is already in use.' );
         }
         $self->createInput( 'text', 'email', 'email', htmlspecialchars( $user->email ) );
         ?><p>Change password</p><?php
         $self->createLabel( 'password', 'Old password' );
-        if ( isset( $password_wrong ) ) {
+        if ( isset( $passwordWrong ) ) {
             $self->createError( 'Old password is incorrect' );
         }
         $self->createInput( 'password', 'password', 'password' );
-        $self->createLabel( 'password_new', 'New password' );
-        if ( isset( $password_new_not_matched ) ) {
+        $self->createLabel( 'passwordNew', 'New password' );
+        if ( isset( $passwordNewNotMatched ) ) {
             $self->createError( 'Passwords do not match' );
         }
-        else if ( isset( $password_new_small ) ) {
+        else if ( isset( $passwordNewSmall ) ) {
             $self->createError( 'Your password should be at least 7 characters long' );
         }
-        $self->createInput( 'password', 'password_new', 'password_new' );
-        $self->createLabel( 'password_repeat', 'Repeat' );
-        $self->createInput( 'password', 'password_repeat', 'password_repeat' );
+        $self->createInput( 'password', 'passwordNew', 'passwordNew' );
+        $self->createLabel( 'passwordRepeat', 'Repeat' );
+        $self->createInput( 'password', 'passwordRepeat', 'passwordRepeat' );
         ?><p>Change country</p><?php
-        $countries_select_array = [ [ 'content' => 'Select Country' ] ];
+        $countriesSelectArray = [ [ 'content' => 'Select Country' ] ];
         foreach ( $countries as $key => $country ) {
-            $countries_select_array[] = [ 'value' => $key + 1, 'content' => $country->name ];
+            $countriesSelectArray[] = [ 'value' => $key + 1, 'content' => $country->name ];
         }
-        $self->createSelect( $countries_select_array, 'countryid' );
+        $self->createSelect( $countriesSelectArray, 'countryid' );
         $self->createSubmit( 'Save settings' );
     } );
 
     $form = new Form( 'image', 'create' );
-    $form->output( function( $self ) use( $image_invalid ) {
+    $form->output( function( $self ) use( $imageInvalid ) {
         $self->createLabel( 'image', 'Upload an avatar' );
-        if ( isset( $image_invalid ) ) {
+        if ( isset( $imageInvalid ) ) {
             $self->createError( "This isn't an image" );
         }
         $self->createInput( 'file', 'image', 'avatar-form' );
