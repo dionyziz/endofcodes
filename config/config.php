5da94095d2c658793b0f8c8b59517a514e12d602
pkakelas <pkakelas@gmail.com>
2014-03-29 15:44:54 +0200

    refactor config

--- config/config.php
+++ config/config.php
@@ -8,25 +8,25 @@
                 'dbname' => 'endofcodes'
             ],
             'files' => [
-                'avatar_extentions' => [ 'jpg', 'png', 'jpeg' ],
-                'flag_extention' => 'png'
+                'avatarExtentions' => [ 'jpg', 'png', 'jpeg' ],
+                'flagExtention' => 'png'
             ],
             'paths' => [
                 'avatar_path' => 'avatars/',
-                'flag_path' => 'static/images/flags/'
+                'flagPath' => 'static/images/flags/'
             ],
-            'pass_min_len' => 7,
+            'passMinLen' => 7,
             'age' => [
                 'min' => 8,
                 'max' => 100
             ],
-            'persistent_cookie' => [
+            'persistentCookie' => [
                 'name' => 'sessionid',
                 'duration' => 60 * 60 * 24 * 365 * 20,
-                'unset_time' => 60 * 60 * 24 * 2
+                'unsetTime' => 60 * 60 * 24 * 2
             ],
-            'forgot_password_exp_time' => 3600 * 24,
-            'cli_max_width' => 120,
+            'forgotPasswordExpTime' => 3600 * 24,
+            'cliMaxWidth' => 120,
             'email' => 'team@endofcodes.com',
             'base' => 'http://localhost/endofcodes/'
         ],
@@ -38,25 +38,25 @@
                 'dbname' => 'endofcodes_test'
             ],
             'files' => [
-                'avatar_extentions' => [ 'jpg', 'png', 'jpeg' ],
-                'flag_extention' => 'png'
+                'avatarExtentions' => [ 'jpg', 'png', 'jpeg' ],
+                'flagExtention' => 'png'
             ],
             'paths' => [
-                'avatar_path' => 'avatars/',
-                'flag_path' => 'static/images/flags/'
+                'avatarPath' => 'avatars/',
+                'flagPath' => 'static/images/flags/'
             ],
-            'pass_min_len' => 7,
+            'passMinLen' => 7,
             'age' => [
                 'min' => 8,
                 'max' => 100
             ],
-            'persistent_cookie' => [
+            'persistentCookie' => [
                 'name' => 'sessionid',
                 'duration' => 60 * 60 * 24 * 365 * 20,
-                'unset_time' => 60 * 60 * 24 * 2
+                'unsetTime' => 60 * 60 * 24 * 2
             ],
-            'forgot_password_exp_time' => 3600 * 24,
-            'cli_max_width' => 120,
+            'forgotPasswordExpTime' => 3600 * 24,
+            'cliMaxWidth' => 120,
             'email' => 'team@endofcodes.com',
             'base' => 'http://localhost/endofcodes/'
         ]
