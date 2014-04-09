d98f705183b252ab391e6bd08f58f0880d14cac5
Vitalis Salis <vitsalis@gmail.com>
2014-04-09 19:29:53 +0300

    Bubble is reversed when it can not appear on top

--- static/script/game/view.js
+++ static/script/game/view.js
@@ -1,21 +1,36 @@
 $( document ).ready( function() {
     $( '.creature' ).mouseover( function () {
-        $( '.infobubble' ).show();
         var id = this.getAttribute( 'data-creatureid' );
         var username = this.getAttribute( 'data-username' );
         var x = this.getAttribute( 'data-x' );
         var y = this.getAttribute( 'data-y' );
         var hp = this.getAttribute( 'data-hp' );
         var maxHp = this.getAttribute( 'data-maxHp' );
+        var $this = $( this );
+        var offsetTop = $this.offset().top - $( '.infobubble' ).height() - 14;
+        var $infobubble = $( '.infobubble' );
+        var positioning;
+        $infobubble.show();
         $( '.player' ).text( username );
         $( '.creatureid' ).text( 'Creature ' + id );
         $( '.location' ).text( x + ', ' + y );
         $( '.numeric' ).text( hp + ' / ' + maxHp );
         $( '.damage' ).css( 'width', Math.floor( 100 * ( maxHp - hp ) / maxHp ) + '%' );
-        $( '.infobubble' ).css( 'top', $( this ).position().top - $( '.infobubble' ).height() - 14 );
-        $( '.infobubble' ).css( 'left', $( this ).position().left - $( '.infobubble' ).width() + 30 );
+        if ( offsetTop < 0 ) {
+            positioning = $this.height() + 14;
+            $infobubble.addClass( 'reversed' );
+        }
+        else {
+            positioning = - $infobubble.height() - 14;
+        }
+        $infobubble.css( 'top', $this.position().top + positioning );
+        $infobubble.css( 'left', $this.position().left - $infobubble.width() + 30 );
     } );
     $( '.creature' ).mouseout( function() {
-        $( '.infobubble' ).hide();
+        var $infobubble = $( '.infobubble' );
+        if ( $infobubble.hasClass( 'reversed' ) ) {
+            $infobubble.removeClass( 'reversed' );
+        }
+        $infobubble.hide();
     } );
 } );
