16193ac40f3b20c01a2ba1b7edf657179c1a211b
Vitalis Salis <vitsalis@gmail.com>
2014-04-10 16:31:54 +0300

    Click next round multiple times

--- static/script/game/view.js
+++ static/script/game/view.js
@@ -36,7 +36,24 @@ $( document ).ready( function() {
     } );
     $( '.next a' ).click( function() {
         $.getJSON( this.href, function( creatures ) {
+            var href = $( '.next a' ).attr( 'href' );
+            var roundid;
+            var attribute;
+            var gameid;
             var maxHp = $( '.creature' ).attr( 'data-maxHp' );
+
+            hrefArray = href.substr( href.indexOf( "?" ) + 1 ).split( "&" );
+            for ( var i = 0; i < hrefArray.length; ++i ) {
+                attribute = hrefArray[ i ].split( "=" );
+                if ( attribute[ 0 ] == 'roundid' ) {
+                    roundid = parseInt( attribute[ 1 ] ) + 1;
+                }
+                else if ( attribute[ 0 ] == 'gameid' ) {
+                    gameid = parseInt( attribute[ 1 ] );
+                }
+            }
+            $( '.next a' ).attr( 'href', "game/view?gameid=" + gameid + "&roundid=" + roundid );
+
             $( '.creature' ).remove();
             for ( var i = 0; i < creatures.length; ++i ) {
                 var creature = creatures[ i ];
