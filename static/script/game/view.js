d812a4efc925c6433937f0b6481e5b90f902ae11
Vitalis Salis <vitsalis@gmail.com>
2014-04-10 15:19:30 +0300

    Use constants

--- static/script/game/view.js
+++ static/script/game/view.js
@@ -1,5 +1,7 @@
 $( document ).ready( function() {
     $( '.creature' ).mouseover( function () {
+        var ARROW_HEIGHT = 14;
+        var ARROW_WIDTH = 30;
         var id = this.getAttribute( 'data-creatureid' );
         var username = this.getAttribute( 'data-username' );
         var x = this.getAttribute( 'data-x' );
@@ -18,14 +20,14 @@ $( document ).ready( function() {
         $( '.numeric' ).text( hp + ' / ' + maxHp );
         $( '.damage' ).css( 'width', Math.floor( 100 * ( maxHp - hp ) / maxHp ) + '%' );
         if ( offsetTop < 0 ) {
-            positioning = $this.height() + 14;
+            positioning = $this.height() + ARROW_HEIGHT;
             $infobubble.addClass( 'reversed' );
         }
         else {
-            positioning = -$infobubble.height() - 14;
+            positioning = -$infobubble.height() - ARROW_HEIGHT;
         }
         $infobubble.css( 'top', $this.position().top + positioning );
-        $infobubble.css( 'left', $this.position().left - $infobubble.width() + 30 );
+        $infobubble.css( 'left', $this.position().left - $infobubble.width() + ARROW_WIDTH );
     } );
     $( '.creature' ).mouseout( function() {
         var $infobubble = $( '.infobubble' );
