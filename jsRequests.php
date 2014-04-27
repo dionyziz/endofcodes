5d461d0d2b9bcb74a14eacf30aae62b7b00d3752
pkakelas <pkakelas@gmail.com>
2014-04-27 20:55:44 +0300

    Working on ajax in form_validation

--- jsRequests.php
+++ jsRequests.php
@@ -1,16 +1,25 @@
 <?php
-
     if ( isset( $_POST[ 'function' ] ) ) {
-        $func = new ReflectionFunction( $_POST[ 'function' ] );
-        foreach( $func->getParameters() as $param ) {
-            if ( isset( $_POST[ $param ] ) ) {
-                $args[ $param ] = $_POST[ $param ];
+        $args = get_func_argNames( $_POST[ 'function' ] );
+        $params = [];
+        foreach ( $args as $arg ) {
+            if ( isset( $_POST[ $arg ] ) ) {
+                $params[] = $_POST[ $arg ];  
             }
         }
-        echo call_user_func_array( $_POST[ 'function' ], $args );
+        echo call_user_func_array( $_POST[ 'function' ], $params );
+    }
+    
+    function checkUsername( $username ) {
+        echo 'fdsa';
     }
 
-    function checkUsername( $username = 'fdsafsa', $email ) {
-        return $username;
+    function get_func_argNames( $funcName ) {
+        $func = new ReflectionFunction( $funcName );
+        $result = array();
+        foreach ( $func->getParameters() as $param ) {
+            $result[] = $param->name;   
+        }
+        return $result;
     }
 ?>
