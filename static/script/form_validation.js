274be0eea42f251f6c61d84e1dd6a3b244ef0dcb
pkakelas <pkakelas@gmail.com>
2014-04-28 18:04:26 +0300

    Finish form validation in register form

--- static/script/form_validation.js
+++ static/script/form_validation.js
@@ -4,26 +4,25 @@ $( document ).ready( function() {
         var $password = $( '#password' ).val();  
         var $passwordRepeat = $( '#password_repeat' ).val();  
         var $email = $( '#email' ).val();  
-        $.ajax({
-            url: 'jsRequests.php',
-            type: 'post',
-            data: { 
-                'function': 'checkUsername', 
-                'username': $username 
-            },
-            success: function(response) { 
-                alert( response ); 
-            }
-        });
         if ( $username == '' ) {
             createError( '#register-form', 'Please type a username.' );
             return false;
-            //window.location.href = "user/create?username_empty=yes";
         }
         if ( !$username.match( /^[a-zA-Z0-9_.]{3,16}$/ ) ) {
             createError( '#register-form', 'Usernames can only have numbers, letters, "." and "_"' );
             return false;
         }
+        $.get(
+            'user/view',
+            {
+                username: $username 
+            },
+            function( responseText ) {
+                    createError( '#register-form', 'Username already exists' );
+                    return false;
+            },
+            "html"
+        );
         if ( $password == '' ) {
             createError( '#register-form', 'Please type a password.' );
             return false;
@@ -49,14 +48,3 @@ $( document ).ready( function() {
         $( $form ).prepend( "<div class='alert alert-danger'>" + $description + "</div>" );
     }
 });
-
-
-
-
-
-
-
-
-
-
-
