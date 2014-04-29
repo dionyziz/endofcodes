7c7ca14505a96b00a98be2530fbba9d06f41da6d
pkakelas <pkakelas@gmail.com>
2014-04-29 19:54:19 +0300

    init commit

--- database/migration/20140408205938_populate_country.php
+++ database/migration/20140408205938_populate_country.php
@@ -1,8 +1,6 @@
 <?php
     require_once 'models/country.php';
-    require_once 'data/countries_array.php';
-
-    $countries = getCountries();
+    $countries = json_decode( file_get_contents( 'database/migration/data/countries.json' ), true );
 
     foreach ( $countries as $countryShortname => $countryName ) {
         $country = new Country();
