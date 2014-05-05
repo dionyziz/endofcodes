190c0c9e69cd1622fa61ca8902c7d0a0216f2b7c
pkakelas <pkakelas@gmail.com>
2014-05-05 17:48:38 +0300

    change the order of functions in helpers/html.php tests

--- tests/helpers/html.php
+++ tests/helpers/html.php
@@ -3,9 +3,8 @@
         public function testCreateSelectPrepare() {
             $test = [ 'a', 'b' ];
             $keys = [ 'key1', 'key2' ];
+            
             $res1 = createSelectPrepare( $test ); 
-            $res2 = createSelectPrepare( $test, '', $keys ); 
-            $res3 = createSelectPrepare( $test, 'testTitle' ); 
             $this->assertTrue( is_array( $res1 ), 
                 'createPrepareSelect must return an array with keys its elements if not any keys are given' );
             $this->assertTrue( isset( $res1[ 'a' ] ), 
@@ -16,10 +15,14 @@
                 'createPrepareSelect must return an array with keys its elements if not any keys are given' );
             $this->assertEquals( $res1[ 'b' ], 'b',  
                 'createPrepareSelect keys must be the same with elements if not any keys are given' );
+
+            $res2 = createSelectPrepare( $test, '', $keys ); 
             $this->assertEquals( $res2[ 'key1' ], 'a',  
                 'createPrepareSelect keys must be the same with elements if not any keys are given' );
             $this->assertEquals( $res2[ 'key2' ], 'b',  
                 'createPrepareSelect must combine the array with the keys in order' );
+
+            $res3 = createSelectPrepare( $test, 'testTitle' ); 
             $this->assertEquals( $res3[ 'first' ], 'testTitle',  
                 'createPrepareSelect must hold in the key "first" the value given as a second parameter' );
             $this->assertEquals( array_shift( array_slice( $res3, 0, 1 ) ), 'testTitle',  
