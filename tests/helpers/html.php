fa48b6dc26db6e21298a5fdb1fd6079de492c7b7
pkakelas <pkakelas@gmail.com>
2014-05-05 17:08:31 +0300

    create regression test for createSelectPrepare()

---
+++ tests/helpers/html.php
@@ -0,0 +1,31 @@
+<?php
+    class htmlTest extends UnitTestWithFixtures {
+        public function testCreateSelectPrepare() {
+            $test = [ 'a', 'b' ];
+            $keys = [ 'key1', 'key2' ];
+            $res1 = createSelectPrepare( $test ); 
+            $res2 = createSelectPrepare( $test, '', $keys ); 
+            $res3 = createSelectPrepare( $test, 'testTitle' ); 
+            $this->assertTrue( is_array( $res1 ), 
+                'createPrepareSelect must return an array with keys its elements if not any keys are given' );
+            $this->assertTrue( isset( $res1[ 'a' ] ), 
+                'createPrepareSelect must return an array with keys its elements if not any keys are given' );
+            $this->assertEquals( $res1[ 'a' ], 'a',  
+                'createPrepareSelect keys must be the same with elements if not any keys are given' );
+            $this->assertTrue( isset( $res1[ 'b' ] ), 
+                'createPrepareSelect must return an array with keys its elements if not any keys are given' );
+            $this->assertEquals( $res1[ 'b' ], 'b',  
+                'createPrepareSelect keys must be the same with elements if not any keys are given' );
+            $this->assertEquals( $res2[ 'key1' ], 'a',  
+                'createPrepareSelect keys must be the same with elements if not any keys are given' );
+            $this->assertEquals( $res2[ 'key2' ], 'b',  
+                'createPrepareSelect must combine the array with the keys in order' );
+            $this->assertEquals( $res3[ 'first' ], 'testTitle',  
+                'createPrepareSelect must hold in the key "first" the value given as a second parameter' );
+            $this->assertEquals( array_shift( array_slice( $res3, 0, 1 ) ), 'testTitle',  
+                'createPrepareSelect must return an array with first element the value given as a second parameter' );
+        } 
+    }
+
+    return new htmlTest();
+?>
