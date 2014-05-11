7a2fa4997616f2fab0b2f888a7681c241aed2ea0
Vitalis Salis <vitsalis@gmail.com>
2014-05-11 20:05:42 +0300

    Implement drop down mockup

--- views/header.php
+++ views/header.php
@@ -26,6 +26,7 @@
             includeStyle( "user/view" );
         ?>
         <script type="text/javascript" src="static/script/jquery-2.1.0.min.js"></script>
+        <script type="text/javascript" src="static/script/bootstrap.min.js"></script>
         <script type="text/javascript" src="static/script/logout.js"></script>
         <script type="text/javascript" src="static/script/game/view.js"></script>
         <script type="text/javascript" src="static/script/prefixfree.min.js"></script>
@@ -39,30 +40,47 @@
                 <div class="navbar-header">
                     <a class="navbar-brand" href="">End Of Codes</a>
                 </div>
-                <ul class="nav navbar-nav">
+                <ul class="nav navbar-nav navbar-left">
                     <li class="active"><a href="">Home</a></li>
                     <li><a href="">Rules</a></li>
                     <li><a href="http://blog.endofcodes.com">Blog</a></li>
                 </ul>
-                <ul class="nav navbar-nav navbar-right"><?php
-                    if ( isset( $_SESSION[ 'user'] ) ) {
-                        ?><li><a 
-                            href="user/view?username=<?php
-                                echo htmlspecialchars( $_SESSION[ 'user' ]->username );
-                            ?> ">Profile</a>
-                        </li>
-                        <li>
-                            <a href="#" id="logout">Log out</a>
-                        </li><?php
-                        $form = new Form( 'session', 'delete' );
-                        $form->id = 'logout-form';
-                        $form->output();
-                    }
-                    else {
-                        ?><li><a href="session/create">Login</a></li>
-                        <li class="active"><a href="user/create">Register</a></li><?php
-                    }
-               ?></ul>
+                <ul class="nav navbar-nav navbar-right">
+                    <?php
+                        if ( isset( $_SESSION[ 'user' ] ) ) {
+                            $user = $_SESSION[ 'user' ];
+                            ?><li><a href="user/view?username=<?php
+                                echo $user->username;
+                            ?>"><img src="<?php
+                                echo $user->image->target_path;
+                            ?>" width="30" height="30" /><?php
+                                echo $user->username;
+                            ?></a></li>
+                            <li class="dropdown">
+                                <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                                    <span class="settings-icon glyphicon glyphicon-cog"></span>
+                                </a>
+                                <ul class="dropdown-menu">
+                                    <li role="presentation">
+                                        <a href="user/update">Settings</a>
+                                    </li>
+                                    <li role="presentation">
+                                        <a href="#" id="logout">Sign out</a>
+                                    </li>
+                                    <?php
+                                        $form = new Form( 'session', 'delete' );
+                                        $form->id = 'logout-form';
+                                        $form->output();
+                                    ?>
+                                </ul>
+                            </li><?php
+                        }
+                        else {
+                            ?><li><a href="session/create">Login</a></li>
+                            <li class="active"><a href="user/create">Register</a></li><?php
+                        }
+                    ?>
+                </ul>
             </div>
         </div>
         <div class="container" id="main">
