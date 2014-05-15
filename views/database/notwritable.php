31807d977b2dd411caed3a6de2a38f882bd30e2f
bugos <baggos12@gmail.com>
2014-05-15 19:28:43 +0300

    rename results.php to notwritable.php

--- views/database/notwritable.php
+++ views/database/notwritable.php
@@ -3,8 +3,8 @@
 ?>
 <p class="bg-warning">We were not able to save the configuration, probably because of insufficient permissions.</p>
 <p>Please copy/paste the following contents in endofcodes/config/config-local.php</p>
-<textarea class="form-control" rows="6"><?php
-	echo $content;
+<textarea class="form-control" rows="8"><?php
+    echo $content;
 ?></textarea>
 <?php
     require 'views/footer.php';
