521989239ef77209e91382af5e35de1784ecef3f
pkakelas <pkakelas@gmail.com>
2014-05-15 14:24:52 +0300

    fix form errors to prepend in a specific input of the form. Also move .focus() to the error function

--- static/script/error.js
+++ static/script/error.js
@@ -1,4 +1,6 @@
-function createError( form, description ) {
+function createFormError( form, inputName, description, focus ) {
+    focus = focus || true;
     $( '.alert' ).remove();
-    $( form ).prepend( "<div class='alert alert-danger'>" + description + "</div>" );
+    $( "<div class='alert alert-danger'>" + description + "</div>" ).insertBefore( form + ' input[name=' + inputName + ']' );
+    $( '[name=' + inputName + ']', form ).focus();
 }
