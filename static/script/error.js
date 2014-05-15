1daa6f727feaf27de77374699662e40c887b94ea
pkakelas <pkakelas@gmail.com>
2014-05-15 16:13:36 +0300

    add select() on error

--- static/script/error.js
+++ static/script/error.js
@@ -2,5 +2,5 @@ function createFormError( form, inputName, description, focus ) {
     focus = focus || true;
     $( '.alert' ).remove();
     $( "<div class='alert alert-danger'>" + description + "</div>" ).insertBefore( form + ' input[name=' + inputName + ']' );
-    $( '[name=' + inputName + ']', form ).focus();
+    $( '[name=' + inputName + ']', form ).focus().select();
 }
