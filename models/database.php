3f1fe916fa15e3ccc4167a7509ec49300d2f5c3f
bugos <baggos12@gmail.com>
2014-05-15 19:37:11 +0300

    create formatConfig() and declare DBExceptionNotWritable

--- models/database.php
+++ models/database.php
@@ -3,47 +3,58 @@
         global $config;
 
         $dbConnected = mysql_connect( $config[ 'db' ][ 'host' ], 
-        	           $config[ 'db' ][ 'user' ], 
-        	           $config[ 'db' ][ 'pass' ] );
-       	if ( !$dbConnected ) { 
-        	throw new DBException( 'Failed to connect to MySQL.', mysql_error() );
-    	}
+                                      $config[ 'db' ][ 'user' ], 
+                                      $config[ 'db' ][ 'pass' ] );
+        if ( !$dbConnected ) { 
+            throw new DBException( 'Failed to connect to MySQL.', mysql_error() );
+        }
         $dbSelected = mysql_select_db( $config[ 'db' ][ 'dbname' ] );
         if ( !$dbSelected ) { 
-        	throw new DBException( 'Failed to select MySQL database.', mysql_error() );
-    	}
+            throw new DBException( 'Failed to select MySQL database.', mysql_error() );
+        }
     } 
     function loadConfig( $environment = '' ) {
-    	$path = 'config/config-local.php';
-    	$config = array();
-		if ( file_exists( $path ) ) {
-            $config = require $path;
+        $path = 'config/config-local.php';
+        $config = array();
+        if ( !file_exists( $path ) ) {
+            return $config;
         }
-        if ( !empty($environment) ) {
-        	$config = $config[ $environment ];
-        	$config = $config[ 'db' ];
+        $config = require $path;
+        if ( !empty( $environment ) ) {
+            $config = $config[ $environment ];
+            $config = $config[ 'db' ];
         }
         return $config;
     }
+    function formatConfig( $config ) {
+        $content = '<?php' . PHP_EOL . 'return ' . var_export( $config, true ) . ';' . PHP_EOL . '?>';
+        $content = str_replace( '  ', '    ', $content ); //convert 2 spaces to 4
+        $content = preg_replace( '/\s*array \(/', ' [', $content );
+        $content = str_replace( ')', ']', $content );
+        return $content;
+    }
     function createConfig( $entries, $environment ) {
-    	$entries = ['db' => $entries];
-    	$entries = [$environment => $entries];
+        $entries = [ 'db'         => $entries ];
+        $entries = [ $environment => $entries ];
 
-    	$path = 'config/config-local.php';
-    	$config = loadConfig();
+        $path = 'config/config-local.php';
+        $config = loadConfig();
         $config = array_replace_recursive( $config, $entries );
 
-    	$content = '<?php' . PHP_EOL . 'return ' . var_export($config, true) . ';';
+        $content = formatConfig( $config );
 
-    	if ( !file_exists($path) ) {
-    		touch( $path );
-    	}
-    	if ( is_writable( $path ) ) {
-    		$configSaved = is_numeric( file_put_contents( $path, $content ) );
-    	}
-    	if ( empty( $configSaved ) ) {
-    		return $content;
-    	}
-    	return false; //Success
+        @touch( $path ); // is_writable will return false if this fails.
+        if ( is_writable( $path ) ) {
+            $configSaved = is_numeric( file_put_contents( $path, $content ) );
+        }
+        if ( empty( $configSaved ) ) {
+            throw new DBExceptionNotWritable( $content );
+        }
+    }
+    class DBExceptionNotWritable extends DBException {
+        public $content;
+        public function __construct( $content ) {
+            $this->content = $content;
+        }
     }
 ?>
