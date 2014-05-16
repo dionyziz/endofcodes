c93af0b9c95f3ebbbb281a91b6fb1ca6cc1c20aa
Dionysis Zindros <dionyziz@gmail.com>
2014-05-16 18:49:08 +0300

    Remove repo icons from footer

--- views/footer.php
+++ views/footer.php
@@ -1,13 +1,5 @@
         </div>
         <footer id="footer" class="panel-footer"> 
-            <ul class="social-buttons">
-                <li>
-                    <iframe class="github-btn" src="http://ghbtns.com/github-btn.html?user=dionyziz&amp;repo=endofcodes&amp;type=watch&amp;count=true" title="Star on GitHub" /></iframe>
-                </li>
-                <li>
-                    <iframe class="github-btn" src="http://ghbtns.com/github-btn.html?user=dionyziz&amp;repo=endofcodes&amp;type=fork&amp;count=true" title="Fork on GitHub"></iframe>
-                </li>
-            </ul>    
         </footer>
     </body>
 </html>
