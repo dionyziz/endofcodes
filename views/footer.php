35f4bd487d3fef87c0fc880a1347f5c5d987abd7
Dionysis Zindros <dionyziz@gmail.com>
2014-05-16 23:23:51 +0300

    Add profiling information to footer

--- views/footer.php
+++ views/footer.php
@@ -1,5 +1,8 @@
         </div>
-        <footer id="footer" class="panel-footer"> 
+        <footer id="footer" class="panel-footer">
+            <div class="dev">dt = <a href=''><?php
+                echo round( 1000 * $this->getPageGenerationTime(), 2 );
+            ?>ms</a></div>
         </footer>
     </body>
 </html>
