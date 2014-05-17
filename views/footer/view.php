b325b212457a23e24f02bd5feb553782d33d4533
Dionysis Zindros <dionyziz@gmail.com>
2014-05-17 16:56:26 +0300

    Merge from refactor_footer

---
+++ views/footer/view.php
@@ -0,0 +1,8 @@
+        </div>
+        <footer id="footer" class="panel-footer">
+            <?php
+                include 'footer/profiling.php';
+            ?>
+        </footer>
+    </body>
+</html>
