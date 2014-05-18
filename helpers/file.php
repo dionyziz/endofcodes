0c49874b1d056e42f5900faa2e6a08ec8a1d0677
bugos <baggos12@gmail.com>
2014-05-18 19:29:17 +0300

    tabs to spaces

--- helpers/file.php
+++ helpers/file.php
@@ -1,9 +1,9 @@
 <?php
     class FileNotWritableException extends Exception {
-    	public $filename;
+        public $filename;
         public $content;
         public function __construct( $filename, $content ) {
-        	$this->filename = $filename;
+            $this->filename = $filename;
             $this->content = $content;
             parent::__construct( 'Failed to write to ' . $filename );
         }
