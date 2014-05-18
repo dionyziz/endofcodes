31825b2734bfa001c32c4f66cf281eae33ebd318
Vitalis Salis <vitsalis@gmail.com>
2014-05-18 15:51:19 +0300

    revokePasswordCheck required a string, int was given

--- tests/models/user.php
+++ tests/models/user.php
@@ -160,7 +160,7 @@
             catch ( ForgotPasswordModelInvalidTokenException $e ) {
                 $oldTokenSuccess = true; 
             }
-            $user->forgotpasswordrequestcreated = time() - 60 * 60 * 24 * 2;
+            $user->forgotpasswordrequestcreated = date( "Y-m-d h:i:s", time() - 60 * 60 * 24 * 2 );
             try {
                 $user->revokePasswordCheck( $user->forgotpasswordtoken );
                 $expiredSuccess = false;
