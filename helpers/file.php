f5ec525a6fa9117aff3d71b76cdb4d1dc8f5dc68
bugos <baggos12@gmail.com>
2014-05-18 19:11:30 +0300

    Create file.php and define FileNotWritableException

---
+++ helpers/file.php
@@ -0,0 +1,11 @@
+<?php
+    class FileNotWritableException extends Exception {
+    	public $filename;
+        public $content;
+        public function __construct( $filename, $content ) {
+        	$this->filename = $filename;
+            $this->content = $content;
+            parent::__construct( 'Failed to write to ' . $filename );
+        }
+    }
+?>
