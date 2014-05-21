ce27671383b46a6daa90e45dc891b90bb5023f4b
Dionysis Zindros <dionyziz@gmail.com>
2014-05-21 18:27:31 +0300

    Rename "profiling" controller to "debugging"

--- controllers/profiling.php
+++
@@ -1,9 +0,0 @@
-<?php
-    class ProfilingController extends AuthenticatedController {
-        public function update( $enable = true ) {
-            $this->requireDeveloper();
-
-            $_SESSION[ 'profiling' ] = ( bool )$enable;
-        }
-    }
-?>
