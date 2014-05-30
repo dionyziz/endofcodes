f6968fe214fd06e20ab2d680bc72c9acce54e7d9
Dionysis Zindros <dionyziz@gmail.com>
2014-05-30 16:32:19 +0300

    Simplify code pertaining to running all testscases in controller

--- controllers/testrun.php
+++ controllers/testrun.php
@@ -3,17 +3,13 @@
         public $environment = 'test';
 
         public function create( $name = '' ) {
-            $all = $name == '';
-
-            if ( $all ) {
-                set_time_limit( 360 );
-            }
-
             require_once 'models/test/base.php';
             require_once 'models/test/functional.php';
             require_once 'models/test/withfixtures.php';
 
-            if ( $all ) {
+            if ( $name == '' ) {
+                set_time_limit( 360 );
+
                 $tests = UnitTest::findAll();
             }
             else {
