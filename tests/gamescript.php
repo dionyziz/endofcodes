a640f27b3ad6e85ce7cca0e1646b7e6528f657c1
Dionysis Zindros <dionyziz@gmail.com>
2014-06-08 12:01:07 +0300

    Change directory to endofcodes folder on gamescript

--- tests/gamescript.php
+++ tests/gamescript.php
@@ -2,14 +2,14 @@
     require_once 'helpers/file.php';
     class GamescriptTest extends FunctionalTest {
         protected $mockPath = 'bots/mock';
+        protected $currentPath;
 
         public function setUp() {
+            $this->currentPath = getcwd();
+
             $mockPath = $this->mockPath;
             recursiveCopy( 'bots/php', $mockPath );
             file_put_contents( $mockPath . '/bot.php', str_replace( 'sample_username', 'sample_username2', file_get_contents( $mockPath . '/bot.php' ) ) );
-        }
-        public function tearDown() {
-            $this->rrmdir( 'bots/mock' );
         }
         public function testGamescript() {
             global $config;
@@ -17,15 +17,22 @@
             $this->buildUser( 'sample_username' );
             $this->buildUser( 'sample_username2', $config[ 'base' ] . $this->mockPath );
             exec( "ENVIRONMENT=test ./gamescript.sh", $output );
-            $created = true;
-            try {
+            $this->assertDoesNotThrow( function() {
                 $lastGame = Game::getLastGame();
-            }
-            catch ( ModelNotFoundException $e ) {
-                $created = false;
-            }
-            $this->assertTrue( $created, 'A game must be created' );
-            $this->assertTrue( $lastGame->ended, 'The game must be finished' );
+                $this->assertTrue( $lastGame->ended, 'The game must be finished' );
+            }, 'ModelNotFoundException', 'A game must be created' );
+        }
+        public function testRunOnDifferentDirectory() {
+            chdir( '../..' );
+            exec( "ENVIRONMENT=test $this->currentPath/gamescript.sh", $output );
+            chdir( $this->currentPath );
+            $this->assertDoesNotThrow( function() {
+                Game::getLastGame();
+            }, 'ModelNotFoundException', 'A game must be created' );
+        }
+        public function tearDown() {
+            chdir( $this->currentPath );
+            $this->rrmdir( 'bots/mock' );
         }
     }
     return new GamescriptTest();
