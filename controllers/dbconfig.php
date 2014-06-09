8fff47f1bf2fd03c4a1809c082e214882b0267a6
bugos <baggos12@gmail.com>
2014-06-09 21:32:21 +0300

    Use require for views instead of require_once

--- controllers/dbconfig.php
+++ controllers/dbconfig.php
@@ -11,7 +11,7 @@
             }
             catch ( FileNotWritableException $e ) {
                 $content = $e->content;
-                require_once 'views/database/notwritable.php';
+                require 'views/database/notwritable.php';
             }
         }
         public function createView( $error, $dbSaid ) {
@@ -25,7 +25,7 @@
             ];
             $oldConfig = array_replace_recursive( $oldConfig, $config );
 
-            require_once 'views/database/create.php';
+            require 'views/database/create.php';
         }
 
     }
