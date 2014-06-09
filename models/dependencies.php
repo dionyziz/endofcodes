7133eb2c7c977231bba8583b4ac38477677887fb
bugos <baggos12@gmail.com>
2014-06-09 21:27:58 +0300

    Fix merge

--- models/dependencies.php
+++ models/dependencies.php
@@ -3,11 +3,11 @@
     require_once 'models/db.php';
     require_once 'models/base.php';
     require_once 'models/user.php';
+    require_once 'models/file.php';
     require_once 'controllers/base.php';
     require_once 'controllers/authentication.php';
     require_once 'helpers/html.php';
     require_once 'helpers/pluralize.php';
     require_once 'helpers/config.php';
-    require_once 'helpers/file.php';
     require_once 'helpers/array.php';
 ?>
