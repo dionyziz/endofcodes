ae2e99041fb5102a8b220f975f7773ccdca77b93
bugos <baggos12@gmail.com>
2014-06-17 11:39:10 +0300

    Fix style

--- controllers/httperror.php
+++ controllers/httperror.php
@@ -1,11 +1,9 @@
 <?php
     class HTTPErrorController extends ControllerBase {
-      public $method = 'view';
 
         public function view( $error, $reason, $header ) {
             header( $header );
             require "views/http/$error.php";
         }
-
     }
 ?>
