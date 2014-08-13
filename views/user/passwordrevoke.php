28ed61f9bd1fbe940f08075518a7f03233528673
pkakelas <pkakelas@gmail.com>
2014-08-13 19:23:25 +0300

    beautify passwordrevoke.php

--- views/user/passwordrevoke.php
+++ views/user/passwordrevoke.php
@@ -2,8 +2,13 @@
     include 'views/header.php';
 ?>
 
-<div><?php 
-    $form = new Form( 'forgotpasswordrequest', 'create' );  
+<div class='text-center'><?php
+    if ( isset( $_SESSION[ 'user' ] ) ) {
+        ?><p class="alert alert-warning logged-notice">You are already logged in</p><?php
+    }
+    ?><h1 id='pass-revoke-title'>Enter your username<small> or email</small><h1><?php
+    $form = new Form( 'forgotpasswordrequest', 'create' );
+    $form->id = 'password-revoke';
     $form->output( function( $self ) use ( $input_empty, $username_not_exists, $email_not_exists ) {
         if ( $input_empty ) {
             $self->createError( "Please enter your username or email" );
@@ -14,14 +19,22 @@
         if ( $username_not_exists ) {
             $self->createError( "This username doesn't exist" );
         }
-        $self->createLabel( 'username', 'Please enter your username or email' );
-        $self->createInput( 'text', 'input', 'username' );
-        $self->createInput( 'submit', '', '', 'Reset your password' );
+        ?><div class="form-group"><?php
+            $self->createInput( 'text', 'username', 'username', '' , [
+                'class' => 'form-control',
+                'placeholder' => 'Username'
+            ] );
+        ?></div><?php
+        ?><div id='submit' class="form-group"><?php
+            $self->createInput( 'submit', '', '', 'Reset your password', [
+                'class' => 'btn btn-primary'
+            ] );
+        ?></div><?php
     } );
 ?>
-</div>
+    <p><a href="session/create">Remembered?</a></p>
 
-<p><a href="user/create">Don't have an account?</a></p>
+</div>
 
 <?php
     include 'views/footer/view.php';
