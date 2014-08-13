2eb4faabc71a5c75a7ff61255ee750aa6d241823
pkakelas <pkakelas@gmail.com>
2014-08-13 16:16:41 +0300

    Debug the python bot
    Make my_id an int for comparison with the creature's userid
    Correct the append of intent to the intents list

--- bots/python/bot.py
+++ bots/python/bot.py
@@ -1,7 +1,6 @@
 import json, random, sys
 from urlparse import parse_qs
 from bottle import route, run, template, request, response, hook
-from pprint import pprint 
 
 @hook('after_request')
 def contentType():
@@ -29,14 +28,13 @@ def round():
         'round': request.forms.get('round'),
         'myid': request.forms.get('myid')
     }
-    pprint(vars)
 
     map = json.loads(vars['map'])
     game_id, my_id = vars['gameid'], vars['myid']
     round, W, H = vars['round'], vars['W'], vars['H']
 
-    my_creatures = [creature for creature in map if creature['userid'] == my_id]
-    enemy_creatures = [creature for creature in map if creature['userid'] != my_id]
+    my_creatures = [creature for creature in map if creature['userid'] == int(my_id)]
+    enemy_creatures = [creature for creature in map if creature['userid'] != int(my_id)]
 
     occupied = set([creature['x'] + creature['y'] * 1j for creature in map])
     enemy_occupied = set([creature['x'] + creature['y'] * 1j for creature in enemy_creatures])
@@ -45,28 +43,27 @@ def round():
     for my_creature in my_creatures:
         possible_moves = set()
         possible_attacks = set()
-        intent = {'creatureid': my_creature['id']}
+        intent = {'creatureid': my_creature['creatureid']}
 
         location = my_creature['x'] + my_creature['y'] * 1j
         directions = {'north': -1j, 'east': 1, 'south': 1j, 'west': -1}
 
         for direction, offset in directions.iteritems():
             moved = location + offset
-            if 0 <= moved.real < W and 0 <= moved.imag < H:
+            if 0<= moved.real < W and 0 <= moved.imag < H:
                 if moved not in occupied:
                     possible_moves.add(direction)
                 if moved in enemy_occupied:
                     possible_attacks.add(direction)
-
         if possible_attacks:
             intent['action'] = 'attack'
-            intent['direction'] = random.choice(possible_attacks.keys())
+            intent['direction'] = random.choice(list(possible_attacks))
         elif possible_moves and random.randint(0, 1) == 0:
             intent['action'] = 'move'
-            intent['direction'] = random.choice(possible_moves.keys())
+            intent['direction'] = random.choice(list(possible_moves))
 
         if 'action' in intent:
-            intents.push(intent)
+            intents.append(intent)
 
     return json.dumps({'intent': intents})
 
