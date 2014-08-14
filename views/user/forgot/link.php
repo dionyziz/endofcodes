b31564186cb14e9a9f6d3ea09300ce8a9f40d581
pkakelas <pkakelas@gmail.com>
2014-08-15 00:47:31 +0300

    beautify link-sent page

--- views/user/forgot/link.php
+++ views/user/forgot/link.php
@@ -2,7 +2,7 @@
     include 'views/header.php';
 ?>
 <div>
-    <p>You will shortly receive an e-mail with instructions to reset your password.</p>
+    <p id='link-sent-p' class='alert alert-success'>You will shortly receive an e-mail with instructions to reset your password.</p>
 </div>
 <?php
     include 'views/footer/view.php';
