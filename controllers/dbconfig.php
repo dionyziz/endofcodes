d47c2b7ca86ef12a569ffba25642bcfd16f74862
Nikatlas <nikatlas@gmail.com>
2015-05-17 19:50:39 +0300

    Changed DBConfigController to run all available migrations.

--- controllers/dbconfig.php
+++ controllers/dbconfig.php
@@ -1,13 +1,14 @@
 <?php
     class DbconfigController extends ControllerBase {
         protected $method = 'create';
-        protected function dbInit() {}
+        // protected function dbInit() {} // deprecated dunno why this was here. 
 
         public function create( $user, $pass, $dbname ) {
             $entries = compact( 'user', 'pass', 'dbname' );
             $entries = [ 'db' => $entries ];
             try {
                 updateConfig( $entries, $this->environment );
+                $this->runMigrations();
                 go();
             }
             catch ( FileNotWritableException $e ) {
@@ -27,6 +28,20 @@
             $oldConfig = array_replace_recursive( $oldConfig, $config );
 
             require 'views/dbconfig/create.php';
+        }
+        private function runMigrations(){
+            require_once 'models/migration.php';
+            $migrations = Migration::findAll();
+            $this->init();
+            foreach ( $migrations as $migration ){
+                try {
+                    require_once 'database/migration/' . $migration;
+                }
+                catch( MigrationException $ex ){
+                    // Maybe change this behaviour to revert changes till now or something...
+                    throw $ex;
+                }
+            }
         }
 
     }
